e0c80c75421552c31592e7a320d6b35a38c7b369
Lawrence Kong <fantasy.adoration@gmail.com>
2025-06-24 10:10:08 -0400

    Update notion.py

--- notion.py
+++ notion.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from notion_client import Client
 from datetime import datetime, timedelta
